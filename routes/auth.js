cc4f2bd0c95fe86799b2bc4ea2cf104f14fdafb0
“jelin.mi” <jelin.mi@gmail.com>
2021-12-15 14:30:05 +0100

    iteration 1+2 done

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,9 @@ const User = require('../models/User.model');
 const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
 
+
+// SIGNUP
+
 // GET route ==> to display the signup form to users
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
@@ -36,7 +39,63 @@ router.post('/signup', (req, res, next) => {
     .catch(error => next(error));
   });
 
+
+// LOGIN
+
+// GET route ==> to display the login form to users
+router.get('/login', (req, res) => res.render('auth/login'));
+
+// POST login route ==> to process form data
+router.post('/login', (req, res, next) => {
+  console.log('SESSION =====> ', req.session);
+
+  const { username, password } = req.body;
+ 
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, username and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ username })     // <== check if there's user with the provided username
+    .then(user => {     // response from DB - doesn't matter if found or not)
+                        // <== "user" here is just a placeholder and represents the response from the DB
+      if (!user) {      // <== if there's no user with provided username, notify the user who is trying to login
+        res.render('auth/login', { errorMessage: 'Username is not registered. Try with other username.' });
+        return;
+      } 
+      
+      // if there's a user, compare provided password
+      // with the hashed password saved in the database
+        else if (bcryptjs.compareSync(password, user.passwordHash)) {
+      // if the two passwords match, render the user-profile.hbs and pass the user object to this view
+        // res.render('users/user-profile', { user });
+
+        //******* SAVE THE USER IN THE SESSION ********//
+        req.session.currentUser = user;
+        res.redirect('/userProfile');
+      } else {
+      // if the two passwords DON'T match, render the login form again and send the error message to the user
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => next(error));     // error handler in case some error occurred while getting the data from the DB)
+});
+
+
+// LOGOUT
+
+router.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    res.redirect('/');
+  });
+});
+
 // render the User's profile page
-router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+router.get('/userProfile', (req, res) => {
+  res.render('users/user-profile', { userInSession: req.session.currentUser });
+});
 
 module.exports = router;
